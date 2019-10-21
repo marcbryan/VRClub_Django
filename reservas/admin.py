e947dd0ba18f389a687eb3f8e86c30bf8d9da077
marcbryan <marcboakyeflores98@gmail.com>
2019-10-21 16:02:12 +0200

    Creando Inlines y Admin

--- reservas/admin.py
+++ reservas/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import Usuario, Reserva, Aula, Material, Reserva_Material
 
-admin.site.register(Usuario)
+
+class ReservaInline(admin.StackedInline):
+    model = Reserva
+    extra = 2
+
+
+class UsuarioAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Información del usuario', {'fields' : ['nombre', 'apellidos', 'edad', 'estudios']}),
+    ]
+    inlines = [ReservaInline]
+
+#TODO: MaterialInline y AulaAdmin
+
+admin.site.register(Usuario, UsuarioAdmin)
 admin.site.register(Reserva)
 admin.site.register(Aula)
 admin.site.register(Material)
